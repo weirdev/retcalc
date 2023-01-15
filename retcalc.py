fd5d0b6be21a464ee999123383d2ed704437dc15
weirdev <weirdev@outlook.com>
2023-01-15 15:50:45 -0500

    Replates retirement values tuple with a class for improved erognomics and typesafety

--- retcalc.py
+++ retcalc.py
@@ -1,11 +1,64 @@
-from typing import Any, List, Tuple
+from enum import Enum
+from typing import Any, Callable, List
 import random
 
 from prompt import choose, takefloat, takeint
 
-RVarsTup = Tuple[float, float, float, float,
-                 Tuple[float, float], Tuple[float, float], int]
-RVarsMut = List[Any]
+
+class RSetting(Enum):
+    EXPENDATURE = 1
+    EMERGENCY = 2
+    FIXED = 3
+    EQUITY = 4
+    INFLATION = 5
+    ERET = 6
+    T = 7
+
+
+class RetirementSettings:
+    def __init__(self, expendature, emergency, fixed, equity, inflation, eret, t):
+        self.expendature = expendature
+        self.emergency = emergency
+        self.fixed = fixed
+        self.equity = equity
+        self.inflation = inflation
+        self.eret = eret
+        self.t = t
+
+    def update_val(self, rsetting: RSetting, op: Callable[[Any], Any]):
+        if rsetting == RSetting.EXPENDATURE:
+            self.expendature = op(self.expendature)
+        elif rsetting == RSetting.EMERGENCY:
+            self.emergency = op(self.emergency)
+        elif rsetting == RSetting.FIXED:
+            self.fixed = op(self.fixed)
+        elif rsetting == RSetting.EQUITY:
+            self.equity = op(self.equity)
+        elif rsetting == RSetting.INFLATION:
+            self.inflation = op(self.inflation)
+        elif rsetting == RSetting.ERET:
+            self.eret = op(self.eret)
+        elif rsetting == RSetting.T:
+            self.t = op(self.t)
+
+    def get_val(self, rsetting: RSetting):
+        if rsetting == RSetting.EXPENDATURE:
+            return self.expendature
+        elif rsetting == RSetting.EMERGENCY:
+            return self.emergency
+        elif rsetting == RSetting.FIXED:
+            return self.fixed
+        elif rsetting == RSetting.EQUITY:
+            return self.equity
+        elif rsetting == RSetting.INFLATION:
+            return self.inflation
+        elif rsetting == RSetting.ERET:
+            return self.eret
+        elif rsetting == RSetting.T:
+            return self.t
+
+    def current_value(self):
+        return self.emergency + self.fixed + self.equity
 
 
 def inflated_val(val: float, r: float, t: int):
@@ -19,51 +72,51 @@ def inflated_payments(payment: float, r: float, t: int) -> float:
     return total
 
 
-def retirement_value(expendature, emergency, fixed, equity, inflation, eret, t) -> RVarsTup:
-    inflation_s = random.gauss(*inflation)
-    eret_s = random.gauss(*eret)
-    equity = (equity - expendature) * (1 + eret_s)
-    expendature = expendature * (1 + inflation_s)
-    fixed = fixed * (1 + inflation[0])
+def retirement_value(retirementSettings: RetirementSettings) -> RetirementSettings:
+    inflation_s = random.gauss(*retirementSettings.inflation)
+    eret_s = random.gauss(*retirementSettings.eret)
+    equity = (retirementSettings.equity -
+              retirementSettings.expendature) * (1 + eret_s)
+    expendature = retirementSettings.expendature * (1 + inflation_s)
+    fixed = retirementSettings.fixed * (1 + retirementSettings.inflation[0])
 
-    if t > 0:
-        return retirement_value(expendature, emergency, fixed, equity, inflation, eret, t - 1)
+    if retirementSettings.t > 0:
+        return retirement_value(RetirementSettings(expendature, retirementSettings.emergency, fixed, equity, retirementSettings.inflation, retirementSettings.eret, retirementSettings.t - 1))
     else:
-        return (expendature, emergency, fixed, equity, inflation, eret, t)
+        return RetirementSettings(expendature, retirementSettings.emergency, fixed, equity, retirementSettings.inflation, retirementSettings.eret, retirementSettings.t)
 
 
-def simulate(rconfig: RVarsTup | RVarsMut, n: int) -> List[RVarsTup]:
+def simulate(retirementSettings: RetirementSettings, n: int) -> List[RetirementSettings]:
     results = []
     for _ in range(n):
-        results.append(retirement_value(*rconfig))
+        results.append(retirement_value(retirementSettings))
     return results
 
 
-"""
+def worst_case(runs: List[RetirementSettings], pmin: float):
+    """
 pmin = tail probablility, ie. 1/100 worst case
 """
-
-
-def worst_case(runs: List[RVarsTup], pmin: float):
-    runs = sorted(runs, key=lambda x: x[1] + x[2] + x[3])
+    runs = sorted(runs, key=lambda rs: rs.emergency + rs.fixed + rs.equity)
     return runs[int(len(runs) * pmin)]
 
 
-def optimize_r_var(rconfig: RVarsTup, r_var_to_opt: int, maximize: bool, pmin: float) -> float:
-    rconfiglst = list(rconfig)
+def optimize_r_var(retirementSettings: RetirementSettings, r_var_to_opt: RSetting, maximize: bool, pmin: float) -> float:
     low = 0
     high = 100
 
-    rconfiglst[r_var_to_opt] = high
-    while (worst_case(simulate(rconfiglst, 10_000), pmin)[3] < 0) ^ maximize:
+    # Find top end of range
+    retirementSettings.update_val(r_var_to_opt, lambda _: high)
+    while (worst_case(simulate(retirementSettings, 10_000), pmin).equity < 0) ^ maximize:
+        # TODO: Update low to previous high
         high = high * 2
-        rconfiglst[r_var_to_opt] = high
+        retirementSettings.update_val(r_var_to_opt, lambda _: high)
 
     diff = high
     while diff > 100:
         mid = low + (diff / 2)
-        rconfiglst[r_var_to_opt] = mid
-        if (worst_case(simulate(rconfiglst, 10_000), pmin)[3] > 0) ^ maximize:
+        retirementSettings.update_val(r_var_to_opt, lambda _: mid)
+        if (worst_case(simulate(retirementSettings, 10_000), pmin).equity > 0) ^ maximize:
             high = mid
         else:
             low = mid
@@ -72,16 +125,16 @@ def optimize_r_var(rconfig: RVarsTup, r_var_to_opt: int, maximize: bool, pmin: f
     return high
 
 
-def r_val_print(expendature, emergency, fixed, equity, inflation, eret, t):
-    print(f"Expendature: ${expendature:,.2f}")
-    print(f"Emergency: ${emergency:,.2f}")
-    print(f"Fixed Income: ${fixed:,.2f}")
-    print(f"Equity: ${equity:,.2f}")
-    print(f"Inflation mean: {inflation[0]*100:.2f}%")
-    print(f"Inflation stdev: {inflation[1]*100:.2f}%")
-    print(f"Equity return mean: {eret[0]*100:.2f}%")
-    print(f"Equity return stdev: {eret[1]*100:.2f}%")
-    print(f"Years left: {t}")
+def r_val_print(retirementSettings: RetirementSettings):
+    print(f"Expendature: ${retirementSettings.expendature:,.2f}")
+    print(f"Emergency: ${retirementSettings.emergency:,.2f}")
+    print(f"Fixed Income: ${retirementSettings.fixed:,.2f}")
+    print(f"Equity: ${retirementSettings.equity:,.2f}")
+    print(f"Inflation mean: {retirementSettings.inflation[0]*100:.2f}%")
+    print(f"Inflation stdev: {retirementSettings.inflation[1]*100:.2f}%")
+    print(f"Equity return mean: {retirementSettings.eret[0]*100:.2f}%")
+    print(f"Equity return stdev: {retirementSettings.eret[1]*100:.2f}%")
+    print(f"Years left: {retirementSettings.t}")
 
 
 def safe_ret_expenditure_prompt():
@@ -102,9 +155,9 @@ def safe_ret_expenditure_prompt():
     wcp = takefloat(
         "Enter tail probability for Monte Carlo simulation (<0.5=worse than average result)", 0, 1)
     print("Simulating 10,000 possible scenarios...")
-    runs = simulate((expenditure, 0, fixed, equity,
+    runs = simulate(RetirementSettings(expenditure, 0, fixed, equity,
                     inflation, eret, t), 10_000)
-    retwealth = sum(worst_case(runs, wcp)[1:4])
+    retwealth = worst_case(runs, wcp).current_value()
     print(f"Estimated new worth at end of earning years: ${retwealth:,.2f}")
 
     print()
@@ -114,8 +167,8 @@ def safe_ret_expenditure_prompt():
 
     print()
     print("Binary searching possible retirement scenarios 10,000 times each...")
-    maxexp = optimize_r_var((0, emergency, fixed, retwealth -
-                            emergency - fixed, inflation, eret, t), 0, True, wcp)
+    maxexp = optimize_r_var(RetirementSettings(0, emergency, fixed, retwealth -
+                            emergency - fixed, inflation, eret, t), RSetting.EXPENDATURE, True, wcp)
     print(f"Maximum safe yearly expendature in retirement: ${maxexp:,.2f}")
 
 
@@ -133,7 +186,7 @@ def savings_required_for_expenditure_prompt():
     print()
     print("Binary searching possible retirement scenarios 10,000 times each...")
     maxexp = optimize_r_var(
-        (expenditure, 0, 0, 0, inflation, eret, t), 3, False, wcp)
+        RetirementSettings(expenditure, 0, 0, 0, inflation, eret, t), RSetting.EQUITY, False, wcp)
     print(f"Minimum safe equity savings for retirement: ${maxexp:,.2f}")
 
 
